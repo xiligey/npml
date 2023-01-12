f1976c999bc862600874d09faa0c5ef943638fbb
chenxilin <635943647@qq.com>
2023-01-12 14:18:11 +0800

    feat: setup

---
+++ npml/__init__.py
@@ -0,0 +1,13 @@
+"""A machine learning package implemented by numpy"""
+
+__author__ = "chenxilin"
+
+__version__ = "0.1"
+
+__all__ = [
+    "anomaly_detection",
+    "association_rules",
+    "cluster",
+    "glm",
+    ""
+]
--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-# 安装文件
+"""安装文件"""
+from setuptools import setup
+
+import npml
+
+metadata = dict(
+    name="npml",
+    maintainer="chenxilin",
+    maintainer_email="635943647@qq.com",
+    description="A machine learning package implemented by numpy",
+    license="MIT License",
+    url="https://github.com/xiligey/npml",
+    download_url="https://pypi.org/project/npml/#files",
+    version=npml.__version__,
+    python_requires=">=3.8",
+    install_requires=["numpy", "setuptools", "pandas", "matplotlib"],
+    tests_require=["pytest"],
+    package_data={"": ["*.csv", "*.gz", "*.txt", "*.pxd", "*.rst", "*.jpg"]},
+    zip_safe=False
+)
+
+setup(**metadata)
